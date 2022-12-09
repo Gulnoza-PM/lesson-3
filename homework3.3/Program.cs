1c9428c36df8332ebae7e59b8cdf455c9bf77d53
Your_name <mgulnoza1805@mail.ru>
2022-12-09 17:11:54 +0500

    comit

---
+++ homework3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Number");
+int Num=int.Parse(Console.ReadLine());
+ int Number=1;
+ Console.WriteLine($"Таблица кубов чисел{Num}, от 1 до {Num} ");
+ while(Number<Num+1)
+ {
+    int result = Number*Number*Number;
+    Console.WriteLine(result+",");
+    Number ++;
+ }
+ 
